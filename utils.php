601b91dac8c458d75384e05d2cd9fbd727e12afd
南鸢晨星 <YS_hutao520@163.com>
2026-02-04 10:35:22 +0800

    feat: 更新颜色映射和样式处理，支持更多基础和扩展颜色

--- utils.php
+++ utils.php
@@ -13,58 +13,64 @@ class Utils
     {
         /* ====== 颜色映射 ====== */
         $colors = [
-            '§0' => '#000000',
-            '§1' => '#0000AA',
-            '§2' => '#00AA00',
-            '§3' => '#00AAAA',
-            '§4' => '#AA0000',
-            '§5' => '#AA00AA',
-            '§6' => '#FFAA00',
-            '§7' => '#AAAAAA',
-            '§8' => '#555555',
-            '§9' => '#5555FF',
-            '§a' => '#55FF55',
-            '§b' => '#55FFFF',
-            '§c' => '#FF5555',
-            '§d' => '#FF55FF',
-            '§e' => '#FFFF55',
-            '§f' => '#FFFFFF',
-            '§g' => '#DDD605',
-            '§h' => '#E3D4D1',
-            '§i' => '#CECACA',
-            '§j' => '#443A3B',
-            '§m' => '#971607',
-            '§n' => '#B4684D',
-            '§p' => '#DEB12D',
-            '§q' => '#11A036',
-            '§s' => '#2CBAA8',
-            '§t' => '#21497B',
-            '§u' => '#9A5CC6'
+            // 基础色
+            '§0' => ['fg' => '#000000', 'bg' => '#000000'], // black
+            '§1' => ['fg' => '#0000AA', 'bg' => '#00002A'], // dark_blue
+            '§2' => ['fg' => '#00AA00', 'bg' => '#002A00'], // dark_green
+            '§3' => ['fg' => '#00AAAA', 'bg' => '#002A2A'], // dark_aqua
+            '§4' => ['fg' => '#AA0000', 'bg' => '#2A0000'], // dark_red
+            '§5' => ['fg' => '#AA00AA', 'bg' => '#2A002A'], // dark_purple
+            '§6' => ['fg' => '#FFAA00', 'bg' => '#402A00'], // gold
+            '§7' => ['fg' => '#AAAAAA', 'bg' => '#2A2A2A'], // gray
+            '§8' => ['fg' => '#555555', 'bg' => '#151515'], // dark_gray
+            '§9' => ['fg' => '#5555FF', 'bg' => '#15153F'], // blue
+            '§a' => ['fg' => '#55FF55', 'bg' => '#153F15'], // green
+            '§b' => ['fg' => '#55FFFF', 'bg' => '#153F3F'], // aqua
+            '§c' => ['fg' => '#FF5555', 'bg' => '#3F1515'], // red
+            '§d' => ['fg' => '#FF55FF', 'bg' => '#3F153F'], // light_purple
+            '§e' => ['fg' => '#FFFF55', 'bg' => '#3F3F15'], // yellow
+            '§f' => ['fg' => '#FFFFFF', 'bg' => '#3F3F3F'], // white
+
+            // 基岩版扩展颜色
+            '§g' => ['fg' => '#DDD605', 'bg' => '#373501'], // minecoin_gold
+            '§h' => ['fg' => '#E3D4D1', 'bg' => '#383534'], // material_quartz
+            '§i' => ['fg' => '#CECACA', 'bg' => '#333232'], // material_iron
+            '§j' => ['fg' => '#443A3B', 'bg' => '#110E0E'], // material_netherite
+            '§m' => ['fg' => '#971607', 'bg' => '#250501'], // material_redstone
+            '§n' => ['fg' => '#B4684D', 'bg' => '#2D1A13'], // material_copper
+            '§p' => ['fg' => '#DEB12D', 'bg' => '#372C0B'], // material_gold
+            '§q' => ['fg' => '#47A036', 'bg' => '#04280D'], // material_emerald
+            '§s' => ['fg' => '#2CBAA8', 'bg' => '#0B2E2A'], // material_diamond
+            '§t' => ['fg' => '#21497B', 'bg' => '#08121E'], // material_lapis
+            '§u' => ['fg' => '#9A5CC6', 'bg' => '#261731'], // material_amethyst
+            '§v' => ['fg' => '#EB7114', 'bg' => '#3B1D05'], // material_resin
         ];
 
         /* ====== {} 展开表 ====== */
         $braceMap = [
-            '{black}'       => '§0',
-            '{dark-blue}'   => '§1',
-            '{dark-green}'  => '§2',
-            '{dark-aqua}'   => '§3',
-            '{dark-red}'    => '§4',
+            '{black}' => '§0',
+            '{dark-blue}' => '§1',
+            '{dark-green}' => '§2',
+            '{dark-aqua}' => '§3',
+            '{dark-red}' => '§4',
             '{dark-purple}' => '§5',
-            '{gold}'        => '§6',
-            '{gray}'        => '§7',
-            '{dark-gray}'   => '§8',
-            '{blue}'        => '§9',
-            '{green}'       => '§a',
-            '{aqua}'        => '§b',
-            '{red}'         => '§c',
-            '{light-purple}'=> '§d',
-            '{pink}'        => '§d',
-            '{yellow}'      => '§e',
-            '{white}'       => '§f',
-            '{reset}'       => '§r',
-            '{bold}'        => '§l',
-            '{italic}'      => '§o',
-            '{enter}'       => "\n"
+            '{gold}' => '§6',
+            '{gray}' => '§7',
+            '{dark-gray}' => '§8',
+            '{blue}' => '§9',
+            '{green}' => '§a',
+            '{aqua}' => '§b',
+            '{red}' => '§c',
+            '{light-purple}' => '§d',
+            '{pink}' => '§d',
+            '{yellow}' => '§e',
+            '{white}' => '§f',
+            '{reset}' => '§r',
+            '{bold}' => '§l',
+            '{italic}' => '§o',
+            '{underline}' => '§n',
+            '{strikethrough}' => '§m',
+            '{enter}' => "\n"
         ];
 
         $text = $raw;
@@ -72,37 +78,84 @@ class Utils
             $text = strtr($text, $braceMap);
         }
 
-        $state  = ['color'=>'', 'bold'=>false, 'italic'=>false, 'underline'=>false, 'strikethrough'=>false];
+        $state = [
+            'color' => '',           // 前景色
+            'bg' => '',              // 阴影色
+            'bold' => false,         // 是否加粗
+            'italic' => false,       // 是否斜体
+            'underline' => false,    // 是否下划线
+            'strikethrough' => false // 是否删除线
+        ];
         $buffer = '';
-        $html   = '';
+        $html = '';
 
-        $flush = function() use (&$state, &$buffer, &$html) {
-            if ($buffer === '') { return; }
-            $style = "color:{$state['color']};";
-            if ($state['bold'])          $style .= 'font-weight:bold;';
-            if ($state['italic'])        $style .= 'font-style:italic;';
-            if ($state['underline'])     $style .= 'text-decoration:underline;';
-            if ($state['strikethrough']) $style .= 'text-decoration:line-through;';
-            $html .= "<span style=\"{$style}\">" . $buffer . "</span>";
+        $flush = function () use (&$state, &$buffer, &$html) {
+            if ($buffer === '') {
+                return;
+            }
+
+            $styles = ["color:{$state['color']}"];
+
+            // 注入 CSS 变量供 text-shadow 使用
+            if ($state['bg']) {
+                $styles[] = "--shadow-color:{$state['bg']}";
+            }
+
+            if ($state['bold'])
+                $styles[] = 'font-weight:bold';
+            if ($state['italic'])
+                $styles[] = 'font-style:italic';
+            if ($state['underline'])
+                $styles[] = 'text-decoration:underline';
+            if ($state['strikethrough'])
+                $styles[] = 'text-decoration:line-through';
+
+            // 处理同时存在 underline 和 strikethrough 的情况
+            if ($state['underline'] && $state['strikethrough']) {
+                $styles[] = 'text-decoration:underline line-through';
+            }
+
+            $styleStr = implode(';', $styles);
+            $html .= "<span style=\"{$styleStr}\">{$buffer}</span>";
             $buffer = '';
         };
 
         $len = mb_strlen($text, 'UTF-8');
         for ($i = 0; $i < $len; $i++) {
             $ch = mb_substr($text, $i, 1, 'UTF-8');
+
             if ($ch === '§' && $i + 1 < $len) {
                 $code = mb_substr($text, ++$i, 1, 'UTF-8');
                 $flush();
+
                 switch ($code) {
-                    case 'r':
-                        $state = ['color'=>'', 'bold'=>false, 'italic'=>false, 'underline'=>false, 'strikethrough'=>false];
+                    case 'r': // reset
+                        $state = [
+                            'color' => '',
+                            'bg' => '',
+                            'bold' => false,
+                            'italic' => false,
+                            'underline' => false,
+                            'strikethrough' => false
+                        ];
                         break;
-                    case 'l': $state['bold']          = true; break;
-                    case 'o': $state['italic']        = true; break;
-                    case 'n': $state['underline']     = true; break;
-                    case 'm': $state['strikethrough'] = true; break;
+                    case 'l':
+                        $state['bold'] = true;
+                        break;
+                    case 'o':
+                        $state['italic'] = true;
+                        break;
+                    case 'n':
+                        $state['underline'] = true;
+                        break;
+                    case 'm':
+                        $state['strikethrough'] = true;
+                        break;
                     default:
-                        $state['color'] = $colors["§{$code}"] ?? $state['color'];
+                        if (isset($colors["§{$code}"])) {
+                            $state['color'] = $colors["§{$code}"]['fg'];
+                            $state['bg'] = $colors["§{$code}"]['bg'];
+                        }
                 }
             } elseif ($ch === "\n") {
                 $flush();
